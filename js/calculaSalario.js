b0830f2ebc9eb7413c09b810d613c7744bd6c2e3
Felipe <felipesou94@gmail.com>
2023-09-14 14:32:36 -0300

    Exercício 5 - Calcular novo salário por cargo

---
+++ js/calculaSalario.js
@@ -0,0 +1,53 @@
+
+const btn = document.getElementById("btn");
+const cargo = document.getElementById("cargos");
+const salario = document.getElementById("salario");
+const container = document.getElementById("container");
+container.classList.add("invisivel")
+
+const componentResult = (salario, novoSalario, aumento) =>
+    `   <p>Salário: R$ ${salario}</p>
+        <p>Novo salário: R$ ${novoSalario}</p>
+        <p>Valor do aumento: R$  ${aumento}</p>
+    `
+
+
+btn.addEventListener("click",function(){
+    event.preventDefault();
+    if(salario.value == ''){
+        alert("Informe um valor válido")
+        salario.focus();
+        return;
+    }
+
+    const cargo = document.getElementById("cargos").value;
+    const salarioFloat = parseFloat(document.getElementById("salario").value);
+    let novoSalario = 0;
+    let valorAumento = 0;
+    
+    switch(cargo) {
+		case "GERENTE":			
+			novoSalario = salarioFloat + (salarioFloat * 0.05);
+			valorAumento = novoSalario - salarioFloat;
+            container.classList.remove("invisivel");
+            container.innerHTML = componentResult(salarioFloat, novoSalario,valorAumento);
+			break;
+		case "SUPERVISOR":
+			novoSalario = salarioFloat + (salarioFloat * 0.08);
+			valorAumento = novoSalario - salarioFloat;
+            container.classList.remove("invisivel");
+            container.innerHTML = componentResult(salarioFloat, novoSalario,valorAumento);
+			break;
+		case "OPERADOR":
+			novoSalario = salarioFloat + (salarioFloat * 0.09);
+			valorAumento = novoSalario - salarioFloat;
+			
+			break;
+		default:
+			novoSalario = salarioFloat + (salarioFloat * 0.10);
+			valorAumento = novoSalario - salarioFloat;
+            container.classList.remove("invisivel");
+            container.innerHTML = componentResult(salarioFloat, novoSalario,valorAumento);
+		}
+   
+})
